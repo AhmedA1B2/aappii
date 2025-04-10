279bd110e72c35463de2e014eea9bd37b7817473
AhmedA1B2 <158824651+AhmedA1B2@users.noreply.github.com>
2025-04-10 12:05:20 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,77 +1,73 @@
-const express = require("express")
-const app =express()
-const bodyP=require("body-parser")
-const compiler=require("compilex")
-const options={ stats: true }
+const express = require("express");
+const app = express();
+const bodyP = require("body-parser");
+const compiler = require("compilex");
+const path = require("path");
+const options = { stats: true };
 
-compiler.init(options)
+compiler.init(options);
 
-app.use(bodyP.json())
-app.use("/codemirror-5.65.18",express.static("/codemirror-5.65.18"))
-app.get("/",function(req,res){
-    res.sendFile("/login.html")
-})
+app.use(bodyP.json());
 
-app.get("/index", function (req, res) {
-    res.sendFile("/index.html");
+// ⬅️ هذا يخلي كل الملفات الثابتة تشتغل (HTML, CSS, JS من مجلد public)
+app.use(express.static(path.join(__dirname, "public")));
+
+// ⬅️ نحدد ملفات HTML باستخدام المسار الصحيح
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "public", "login.html"));
 });
 
-app.post("/compile",function(req,res){
-    var code=req.body.code
-    var input=req.body.input
-    var lang=req.body.lang
-    
-    try{
-        if(lang=="CPP"){
-            if(!input){
-                var envData = { OS : "windows" , cmd : "g++",options:{timeout:10000}};
-                compiler.compileCPP(envData , code , function (data) {
+app.get("/index", function (req, res) {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// خدمة كود الكومبايل
+app.post("/compile", function (req, res) {
+    const code = req.body.code;
+    const input = req.body.input;
+    const lang = req.body.lang;
+
+    try {
+        if (lang === "CPP") {
+            const envData = { OS: "windows", cmd: "g++", options: { timeout: 10000 } };
+            if (!input) {
+                compiler.compileCPP(envData, code, function (data) {
                     res.send(data);
-                    //data.error = error message 
-                    //data.output = output value
                 });
-            }else{
-                var envData = { OS : "windows" , cmd : "g++",options:{timeout:10000}}; 
-                compiler.compileCPPWithInput(envData , code , input , function (data) {
+            } else {
+                compiler.compileCPPWithInput(envData, code, input, function (data) {
                     res.send(data);
                 });
             }
-        }else if(lang=="Java"){
-                if(!input){
-                    var envData = { OS : "windows"}; 
-                    compiler.compileJava( envData , code , function(data){
-                        res.send(data);
-                    });    
-                }else{
-                    var envData = { OS : "windows"}; 
-                    compiler.compileJavaWithInput( envData , code , input ,  function(data){
-                        res.send(data);
-                    });
-                }
-                    
-                
-        }else if(lang=="Python"){
-                if(!input){
-                    var envData = { OS : "windows"}; 
-                    compiler.compilePython( envData , code , function(data){
-                        res.send(data);
-                    });    
-                }else{
-                    var envData = { OS : "windows"}; 
-                    compiler.compilePythonWithInput( envData , code , input ,  function(data){
-                        res.send(data);        
-                    });
-                }
-                    
-                
+        } else if (lang === "Java") {
+            const envData = { OS: "windows" };
+            if (!input) {
+                compiler.compileJava(envData, code, function (data) {
+                    res.send(data);
+                });
+            } else {
+                compiler.compileJavaWithInput(envData, code, input, function (data) {
+                    res.send(data);
+                });
             }
-    }  
-    catch(e){
+        } else if (lang === "Python") {
+            const envData = { OS: "windows" };
+            if (!input) {
+                compiler.compilePython(envData, code, function (data) {
+                    res.send(data);
+                });
+            } else {
+                compiler.compilePythonWithInput(envData, code, input, function (data) {
+                    res.send(data);
+                });
+            }
+        }
+    } catch (e) {
         console.log("Compilation Error:", e);
         res.send({ error: "Compilation Error", details: e });
-    }    
-})
+    }
+});
 
-
-
-app.listen(8000)
+app.listen(8000, () => {
+    console.log("Server running on port 8000");
+});
